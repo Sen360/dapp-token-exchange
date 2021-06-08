bb027c12cdfbf8b9915a2fd0db1b91b91109fb71
Senade <lokosenade@gmail.com>
2021-06-08 14:07:13 +0200

    make it better

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './components/App'
+import configureStore from './store/configureStore'
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+
